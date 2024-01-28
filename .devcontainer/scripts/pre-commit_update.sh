9001d11a8f5e95090cb853af0d40fbc73cb054bc
Tom <dev@tomblanc.fr>
2024-01-28 16:28:10 +0000

    Adding script to update pre-commit

---
+++ .devcontainer/scripts/pre-commit_update.sh
@@ -0,0 +1,19 @@
+cd /workspace
+
+CYAN='\033[0;36m'
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+echo -e "${CYAN}Updating pre-commit...${NC}"
+pre-commit install
+
+echo -e "${CYAN}Updating pre-commit hooks...${NC}"
+pre-commit autoupdate
+
+echo -e "${CYAN}Installing pre-commit hooks...${NC}"
+pre-commit install-hooks
+
+echo -e "${GREEN}pre-commit updated successfully!${NC}"
+
+exit 0
