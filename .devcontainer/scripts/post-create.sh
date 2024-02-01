ed23209cde2b30f1e0c3a6b5a62c2023d9c72662
Tom <dev@tomblanc.fr>
2024-02-01 17:16:28 +0000

    Adding easy nginx configuration

--- .devcontainer/scripts/post-create.sh
+++ .devcontainer/scripts/post-create.sh
@@ -1,5 +1,6 @@
 CYAN='\033[0;36m'
 YELLOW='\033[0;33m'
+GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
 echo -e "${CYAN}Runing post-create.sh${NC}"
@@ -7,5 +8,14 @@ echo -e "${CYAN}Runing post-create.sh${NC}"
 cd /workspace
 
 bash /workspace/.devcontainer/scripts/git/add-remote-templates.sh
+
+echo -e "${CYAN}Copying nginx.conf${NC}"
+bash /workspace/.devcontainer/scripts/copy-nginx-conf.sh
+echo -e "${GREEN}Copying nginx.conf done${NC}"
+
+# Start Nginx
+echo -e "${CYAN}Starting Nginx...${NC}"
+service nginx restart
+echo -e "${GREEN}Nginx started${NC}"
 
 exit 0
